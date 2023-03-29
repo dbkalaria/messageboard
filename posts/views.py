3399629b61aceeaf21669b45b2f7419be3aa3f11
dhruv <dhruvkalaria312@gmail.com>
2023-03-29 20:34:08 +0530

    Add view.

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views import View
+
 
 from .models import Post
 
@@ -8,3 +11,11 @@ class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_post_list'
+
+
+class Tooltestview(View):
+    def get(self, request):
+        a  = request.query_params.get("num_1", None)
+        b = request.query_params.get("num_2", None)
+        c = a/b
+        return HttpResponse(c)
